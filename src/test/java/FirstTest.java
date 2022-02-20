3dc65c733b5ffaca269ae17f6134bb4590d94a44
Mani Movassagh <mani.mgh@gmail.com>
2022-02-20 23:06:55 +0100

    add some selector

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -32,6 +33,8 @@ public class FirstTest {
     @Test
     public void testTitle() {
         String expect ="coding blog - Coding Solo | programmierblog | tutorials | Videos";
+
+        driver.findElement(By.cssSelector("#header_main > div > div > span > a > img")).click();
         assertEquals(expect, driver.getTitle());
     }
 }
