a616320f8d09fd649772d90067c934dc7f48197a
Mani Movassagh <mani.mgh@gmail.com>
2022-02-20 23:00:44 +0100

    first part done

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstTest {
+
+    WebDriver driver;
+
+
+    @Before
+    public void beforeTest(){
+
+        System.out.println("Start The TEst Automation");
+        System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://codingsolo.de");
+
+    }
+
+
+
+    @AfterEach
+    void afterTests() {
+        System.out.println("Test abgeschlossen. - Aufräumen");
+        driver.close();
+    }
+
+    @Test
+    public void testTitle() {
+        String expect ="coding blog - Coding Solo | programmierblog | tutorials | Videos";
+        assertEquals(expect, driver.getTitle());
+    }
+}
